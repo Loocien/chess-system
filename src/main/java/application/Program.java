0d0dca754c39cd5ef405456186985cf039317e58
Loocien <nicolassilveiracorrea@gmail.com>
2024-03-17 14:58:03 -0300

    first commit

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+import boardgame.Position;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Position position = new Position(1, 5);
+        System.out.println("a");
+
+    }
+
+}
