bc964fcd32a03046831f2ffff06e9dffae8aeaf3
Loocien <nicolassilveiracorrea@gmail.com>
2024-03-18 18:43:00 -0300

    Possible moves logic

--- src/main/java/boardgame/Piece.java
+++ src/main/java/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
     protected Position position;
     private Board board;
@@ -12,6 +12,26 @@ public class Piece {
 
     protected Board getBoard() {
         return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    //Verifica cada possível movimento de cada posição
+    public boolean possibleMove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    //Verifica se tem pelo menos UM movimento possível
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] mat = possibleMoves();
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat.length; j++) {
+                if (mat[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
